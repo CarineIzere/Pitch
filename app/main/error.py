9ddff0250a470931b533bf9fd104cefba5cf986d
CarineIzere <carizeree@gmail.com>
2019-02-28 09:53:07 +0200

    add function to render the 404 error page

--- app/main/error.py
+++ app/main/error.py
@@ -5,7 +5,7 @@ from . import main
 @main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
-    function to render the 404 error page
+    function to render the 404 error page.
     '''
 
     return render_template("fourOwfour.html"),404
